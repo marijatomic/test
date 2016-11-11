bbab80f7401a15513706705b0a5ac4e3ad95c8c2
marijatuka <marijat1995@gmail.com>
2016-11-11 11:03:38 +0100

    Prvi commit

---
+++ app/src/main/java/ba/sve_mo/fpmoz/marija/testni/MainActivity.java
@@ -0,0 +1,13 @@
+package ba.sve_mo.fpmoz.marija.testni;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
